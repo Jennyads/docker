564084954b492bb04a15114f3e08c03eb3663630
Jennyads <jennycristina.1998@gmail.com>
2024-01-29 14:29:24 -0300

    update projects

--- 4_YAML/app.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-if __name__ == '__main__':
-
-    stream = open("test.yaml", "r")
-    dictionary = yaml.safe_load(stream)
-
-    for key, value in dictionary.items():
-        print(key + " : " + str(value))
